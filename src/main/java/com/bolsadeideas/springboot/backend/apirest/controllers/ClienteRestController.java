b950364a596607d0464462cf6f4630031900fb0e
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-04-23 20:48:42 -0500

    Creando los metodos show y create en el controlador de Backend

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,10 +3,8 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,16 @@ public class ClienteRestController {
     @GetMapping("/clientes")
     public List<Cliente> index(){
         return clienteService.findAll();
+    }
+
+    @GetMapping("/clientes/{id}")
+    public Cliente show(@PathVariable Long id){
+        return clienteService.findById(id);
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteService.save(cliente);
     }
 }
