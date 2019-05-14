b49c86d14254b39c9e1ec9572c129087645c0d46
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-05-13 21:10:04 -0500

    Manejo de error en el Backend en metodo handler show

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,10 +3,14 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -22,8 +26,22 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Cliente show(@PathVariable Long id){
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id){
+        Cliente cliente;
+        Map<String, Object> response = new HashMap<>();
+        try{
+            cliente = clienteService.findById(id);
+
+        } catch(DataAccessException e){
+            response.put("mensaje", "Error al realizar consulta en base de datos!");
+            response.put("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (cliente == null){
+            response.put("mensaje", "El cliente ID: ".concat(" no existe en base de datos"));
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/clientes")
