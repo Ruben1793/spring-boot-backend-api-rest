f7ab515b9430718d46e1fdad80bd4a637d090198
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-05-16 21:37:10 -0500

    Implementando anotacion @Valid en metodos create y update de controller

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -45,9 +50,23 @@ public class ClienteRestController {
     }
 
     @PostMapping("/clientes")
-    public ResponseEntity<?> create(@RequestBody Cliente cliente){
+    public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result){
         Cliente clienteNew;
         Map<String, Object> response = new HashMap<>();
+        if (result.hasErrors()){
+            /*
+            List<String> errors = new ArrayList<>();
+            for (FieldError error: result.getFieldErrors()) {
+                errors.add("El campo '" + error.getField() + "' " + error.getDefaultMessage());
+            }
+            */
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(error -> "El campo '" + error.getField() + "' " + error.getDefaultMessage())
+                    .collect(Collectors.toList());
+            response.put("errors", errors);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
         try{
             clienteNew = clienteService.save(cliente);
         } catch(DataAccessException e){
@@ -61,9 +80,17 @@ public class ClienteRestController {
     }
 
     @PutMapping("/clientes/{id}")
-    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id){
+    public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result ,@PathVariable Long id){
         Cliente clienteActual = clienteService.findById(id);
         Map<String, Object> response = new HashMap<>();
+        if (result.hasErrors()){
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(error -> "El campo '" + error.getField() + "' " + error.getDefaultMessage())
+                    .collect(Collectors.toList());
+            response.put("errors", errors);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
         Cliente clienteUpdated = null;
         if (clienteActual == null){
             response.put("mensaje", "Error: no pudo editar, el cliente ID: ".concat(" no existe en base de datos"));
